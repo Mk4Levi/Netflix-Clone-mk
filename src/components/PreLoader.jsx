0fec87eedbe1c4783d68a792e5490e154b9797cc
Manthan <manthansoul@gmail.com>
2023-07-05 11:57:29 +0530

    created PreLoader for app

---
+++ src/components/PreLoader.jsx
@@ -0,0 +1,18 @@
+//
+import "../scss/PreLoader.css";
+
+const PreLoader = () => {
+  return (
+    <div id="page">
+      <div id="container">
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="h3">Loading...</div>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
